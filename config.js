d5d58df8c2e3d298653f29a36135aa934ab11e70
Shahith12 <117558946+Shahith12@users.noreply.github.com>
2023-05-13 16:04:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94760400155', 'SHAHITJ', true],
+  ['94760400155', 'SHAHITH DASANAYAKA', true], 
   [''] 
 ] //Number of owners
 
